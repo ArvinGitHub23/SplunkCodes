6e230f338275fdc312a4f4f240236ae751c1f5c6
ArvinGitHub23 <arvingithub23@gamil.com>
2023-01-10 20:58:18 +0530

    4 methods created

---
+++ src/Practice/Employee.java
@@ -0,0 +1,20 @@
+package Practice;
+
+public class Employee {
+	public void Tc1() {
+		System.out.println("1234567");
+	}
+
+	public void Tc2() {
+		System.out.println("1234567");
+	}
+
+	public void Tc3() {
+		System.out.println("1234567");
+	}
+
+	public void Tc4() {
+		System.out.println("1234567");
+	}
+
+}
